f516419a453446dc9cb987efb33a662f345e8397
Camila Diniz <ddevcamila@gmail.com>
2025-06-19 11:19:42 -0300

    atualiza apenas pasta src

--- src/App.tsx
+++ src/App.tsx
@@ -1,35 +1,14 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
-
-function App() {
-  const [count, setCount] = useState(0)
-
-  return (
-    <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
+export function App () {
+    return (
+        <>
+        <h1>Olá mundo!</h1>Add commentMore actions
         <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
+            Lorem ipsum dolor sit, amet consectetur adipisicing elit. Earum cumque
+            incidunt accusamus commodi quasi sit, doloribus in fugiat, repellat
+            laudantium voluptas praesentium labore dolore unde! Aliquam, dolorem! A,
+            quia rerum!
         </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
     </>
-  )
-}
+  );
+};
 
-export default App
